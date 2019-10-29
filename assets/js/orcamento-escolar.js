5c8985b592051e444511bb7c9355c7899b3402a0
Eduardo Gava <eduardoxdddd@hotmail.com>
2019-10-28 21:50:05 -0300

    28/10

--- assets/js/orcamento-escolar.js
+++ assets/js/orcamento-escolar.js
@@ -1,7 +1,69 @@
-const TIPO_MATERIAL = ["Caneta", "Lapis", "Caderno", "Borracha", "Apontador", "Régua", "Corretivo", "Marca-texto", "Cola", "Tesoura" ];
+const TIPO_MATERIAL = ["Caneta", "Lapis", "Caderno", "Borracha", "Apontador", "Régua", "Corretivo", "Marca-texto", "Cola", "Tesoura"];
 const materialEscolar = document.getElementById("escolhaOpcao");
+var item = {};
 
 TIPO_MATERIAL.forEach((material) => {
-    option = new Option(material, material.toLowerCase());
-    materialEscolar.options[materialEscolar.options.length] = option;
-  });
+  option = new Option(material, material.toLowerCase());
+  materialEscolar.options[materialEscolar.options.length] = option;
+});
+
+function botaoDesabilitar() {
+  if ($("#nomeAluno").valid() && $("#gastarAte").valid()) {
+    $("#botaozao").attr("disabled", false);
+  }
+  else {
+    $("#botaozao").attr("disabled", true);
+  }
+
+}
+
+$("#nomeAluno").focusout(function () {
+  botaoDesabilitar();
+
+});
+
+$("#gastarAte").focusout(function () {
+  botaoDesabilitar();
+});
+
+function validarProdutos() {
+  console.log("validarProdutos");
+
+  return $('#escolhaOpcao').valid() &&
+    $('#quantidadeProduto').valid() &&
+    $('#precoUnitario').valid();
+}
+
+function calcularTotal(Quantidade, Preco) {
+  console.log("calcularTotal");
+
+  return Quantidade * Preco;
+}
+
+function adicionarItem() {
+
+  console.log("adicionarItem");
+  var validarItem = validarProdutos();
+  console.log("validarProdutos");
+
+  if (validarItem) {
+    item.Opcao = $("#escolhaOpcao").val();
+    item.Quantidade = parseInt($("#quantidadeProduto").val());
+    item.Preco = parseInt($("#precoUnitario").val());
+    item.Total = calcularTotal(item.Quantidade, item.Preco);
+    adicionarProdutoTabela(item);
+
+    item = {};
+    $(":input").val("");
+  }
+}
+
+function adicionarProdutoTabela(objeto) {
+  console.log("adicionarProdutoTabela");
+  var tabela = document.querySelector("table");
+  var tr = tabela.insertRow();
+  tr.innerHTML = "<td>" + objeto.Opcao + "</td>" +
+    "<td>" + objeto.Quantidade + "</td>" +
+    "<td>" + objeto.Preco + "</td>" +
+    "<td>" + objeto.Total + "</td>";
+}
